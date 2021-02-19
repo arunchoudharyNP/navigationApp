422698a2f66104871593d1a585c58813de306b0c
arunchoudharyNP <ruchoudhary91.ac@gmail.com>
2021-02-19 11:10:07 +0530

    19 -Feb

--- Components/helpingComponents/InputCom.js
+++ Components/helpingComponents/InputCom.js
@@ -1,22 +1,80 @@
-import React from "react";
-import { Text, View, TextInput, StyleSheet } from "react-native";
+import React, { useState } from "react";
+import { Text, View, TextInput, StyleSheet,TouchableOpacity } from "react-native";
+import { MaterialCommunityIcons } from "@expo/vector-icons";
+
 
 const InputCom = (props) => {
-  const { borderBottom, title, titleStyle,border, design, ...restProps } = props;
+  const {
+    borderBottom,
+    title,
+    titleStyle,
+    border,
+    design,
+    icon,
+    min,
+    max,
+    secure,
+    ...restProps
+  } = props;
+  const [iconName, seticonName] = useState(true);
+
+  const renderIcon = () => {
+    return (
+      <View style={styles.iconStyle}>
+        <MaterialCommunityIcons name={icon} size={28} color="black" />
+      </View>
+    );
+  };
+
+  const passwordViewHandler = () => {
+    console.log(iconName);
+    seticonName(!iconName);
+  };
+
+  const renderEyeButton = () => {
+    return (
+      <View>
+        <TouchableOpacity
+          style={styles.eyeButton}
+          onPress={() => {
+            passwordViewHandler();
+          }}
+        >
+          <MaterialCommunityIcons
+            name={iconName ? "eye" : "eye-off"}
+            color="grey"
+            size={20}
+          />
+        </TouchableOpacity>
+      </View>
+    );
+  };
 
   const inputStyle = [
     styles.input,
     borderBottom && { borderBottomWidth: 1, borderColor: "grey" },
-    border && { borderWidth: 1, borderColor : "grey"}
+    border && { borderWidth: 1, borderColor: "grey" },
   ];
 
   return (
-    <View style={design}>
-      {title && <Text style={[{ marginLeft: 25 }, titleStyle]}> {title} </Text>}
-      <TextInput
-        style={[{ alignSelf: "center" }, inputStyle]}
-        {...restProps}
-      ></TextInput>
+    <View style={[design, { width: "80%" }]}>
+   
+      <View style={{ marginLeft: 30 }}>
+        {title && <Text style={[{}, titleStyle]}> {title} </Text>}
+        {icon && renderIcon()}
+        <View style={{ width: "100%" }}>
+          <TextInput
+            secureTextEntry={secure && iconName}
+            style={[
+              { alignSelf: "center" },
+              icon ? { paddingLeft: 40 } : null,
+              inputStyle,
+            ]}
+            {...restProps}
+          ></TextInput>
+          {secure && renderEyeButton()}
+        </View>
+      </View>
     </View>
   );
 };
@@ -24,7 +82,17 @@ const InputCom = (props) => {
 const styles = StyleSheet.create({
   input: {
     height: 35,
-    width: "80%",
+    width: "100%",
+  },
+  iconStyle: {
+    position: "absolute",
+    left: 10,
+    top: 36,
+  },
+  eyeButton: {
+    position: "absolute",
+    right: 10,
+    bottom: 8,
   },
 });
 
--- screens/EditProfileScreen.js
+++ screens/EditProfileScreen.js
@@ -9,37 +9,93 @@ import {
 import InputCom from "../Components/helpingComponents/InputCom";
 
 const EditProfileScreen = (props) => {
+  let platformWeb = Platform.OS == "web";
   return (
     <View style={{ backgroundColor: "white", flex: 1 }}>
       <ImageBackground
         style={{ flex: 1 }}
-        source={
-          Platform.OS == "web" ? require("../assets/Background_img.jpg") : null
-        }
+        source={platformWeb ? require("../assets/Background_img.jpg") : null}
       >
-        <View
-          style={Platform.OS == "web" ? styles.webStyle : styles.mobileStyle}
-        >
-          <View style={  Platform.OS == "web"? { flex: 1 } :{} }>
-            <View style={ Platform.OS == "web" ? { flexDirection: "row" } : {}}>
+        <View style={platformWeb ? styles.webStyle : styles.mobileStyle}>
+          <View style={platformWeb ? { flex: 1 } : {}}>
+            <View
+              style={
+                platformWeb
+                  ? { flexDirection: "row" }
+                  : { flexDirection: "column" }
+              }
+            >
               <InputCom
-                design={{ marginVertical: 10, flex:1 }}
+                design={[
+                  { marginVertical: 10 },
+                  platformWeb ? { flex: 1 } : null,
+                ]}
+                maxLength={15}
+                minLength={3}
+                icon={"account"}
                 title="First Name"
                 titleStyle={{ fontSize: 24 }}
                 border
               />
               <InputCom
-                design={{ marginVertical: 10,flex: 1 }}
+                design={[
+                  { marginVertical: 10 },
+                  platformWeb ? { flex: 1 } : null,
+                ]}
+                icon={"account-box"}
                 title="Last Name"
                 titleStyle={{ fontSize: 24 }}
                 border
               />
             </View>
+               
+
+            <View
+              style={
+                platformWeb
+                  ? { flexDirection: "row" }
+                  : { flexDirection: "column" }
+              }
+            >
+              <InputCom
+                design={[
+                  { marginVertical: 10 },
+                  platformWeb ? { flex: 1 } : null,
+                ]}
+                maxLength={15}
+                minLength={3}
+                secure
+                icon={"account-lock"}
+                title="Password"
+                titleStyle={{ fontSize: 24 }}
+                border
+              />
+              <InputCom
+                design={[
+                  { marginVertical: 10 },
+                  platformWeb ? { flex: 1 } : null,
+                ]}
+                secure
+                icon={"account-lock"}
+                title="Confirm Password"
+                titleStyle={{ fontSize: 24 }}
+                border
+              />
+            </View>
+
+
+
+
+
           </View>
-          <View style={ Platform.OS == "web"? { flex: 1 } :{}}>
+          <View style={Platform.OS == "web" ? { flex: 1 } : {}}>
             <InputCom
-              design={{ marginVertical: 10 }}
+              design={[
+                { marginVertical: 10 },
+                platformWeb ? { flex: 1 } : null,
+              ]}
               title="Name"
+              icon={"account-check"}
               titleStyle={{ fontSize: 24 }}
               border
             />
@@ -51,7 +107,9 @@ const EditProfileScreen = (props) => {
 };
 
 const styles = StyleSheet.create({
-  mobileStyle: {},
+  mobileStyle: {
+    flex: 1,
+  },
 
   webStyle: {
     margin: "10%",
